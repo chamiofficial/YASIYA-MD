6fb1cf429ca3b76e0b646d4618756d0768666975
I-AM-CHAMI <88298450+chamiofficial@users.noreply.github.com>
2026-04-15 19:04:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?8aPY8z',
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?bLEsNk',
     PORT: process.env.PORT || 8000,
     SESSION_NAME: process.env.SESSION_NAME || "auth_info_baileys"
 };
